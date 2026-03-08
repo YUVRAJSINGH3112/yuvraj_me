13de97ed2d7859a435f5b0df9789512f69e421d8
YUVRAJSINGH3112 <yuvrajsingh3112s@gmail.com>
2026-03-08 21:17:56 +0530

    03:Navbar Complete Setup

--- components/pages/Navbar.tsx
+++ components/pages/Navbar.tsx
@@ -1,10 +1,18 @@
 "use client"
-
+import Link from "next/link"
 import { useState } from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { ThemeToggleButton } from "@/components/ui/ThemeToggleButton"
-import { Menu, X } from "lucide-react"
+import { Menu, X, Home, BriefcaseBusiness, Contact2 } from "lucide-react"  
 import { motion, AnimatePresence } from "framer-motion"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function Navbar() {
 
@@ -32,6 +40,12 @@ export default function Navbar() {
           cursor-pointer hover:bg-white/15 transition-all active:scale-95"
         >
 
+    <DropdownMenu onOpenChange={setIsOpen}>
+
+      <DropdownMenuTrigger asChild>
+
+        <button className="p-2">
+
           <AnimatePresence mode="wait">
 
             {isOpen ? (
@@ -42,7 +56,7 @@ export default function Navbar() {
                 exit={{ rotate: 90, opacity: 0, scale: 0.8 }}
                 transition={{ duration: 0.25 }}
               >
-                <X size={20} className="text-white/80" />
+                <X size={22} className="text-white/80" />
               </motion.div>
             ) : (
               <motion.div
@@ -52,11 +66,46 @@ export default function Navbar() {
                 exit={{ rotate: -90, opacity: 0, scale: 0.8 }}
                 transition={{ duration: 0.25 }}
               >
-                <Menu size={20} className="text-white/80" />
+                <Menu size={22} className="text-white/80" />
               </motion.div>
             )}
 
           </AnimatePresence>
+
+        </button>
+
+      </DropdownMenuTrigger>
+
+
+      <DropdownMenuContent className="w-40" align="end">
+
+        <DropdownMenuGroup>
+
+          <DropdownMenuItem>
+             <Link href="/" className="flex items-center gap-2">
+              <Home className="h-8 w-8"/>
+              Home
+            </Link>
+          </DropdownMenuItem>
+
+          <DropdownMenuItem asChild>
+            <Link href="/projects" className="flex items-center gap-2">
+              <BriefcaseBusiness className="h-8 w-8"/>
+              Projects
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+
+        <DropdownMenuItem>
+          <Link href="/contact" className="flex items-center gap-2">
+              <Contact2 className="h-8 w-8"/>
+              Contact
+          </Link>
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+
+    </DropdownMenu>
 
         </div>
 
